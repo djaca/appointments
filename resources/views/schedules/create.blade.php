b31d6972dc93b82f7af3c62b01f1a3893703a504
Aleksandar Savanovic <djaca85@gmail.com>
2018-08-02 18:26:55 +0200

    Fix employee select layout in schedules.create

--- resources/views/schedules/create.blade.php
+++ resources/views/schedules/create.blade.php
@@ -31,8 +31,8 @@
                         <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
                       </svg>
                     </div>
-                    {!! $errors->first('employee_id', '<span class="text-red-dark text-sm mt-2">:message</span>') !!}
                   </div>
+                  {!! $errors->first('employee_id', '<span class="text-red-dark text-sm mt-2">:message</span>') !!}
                 </div>
 
                 <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
